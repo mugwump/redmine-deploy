ec6124778a71e18c3c1c29ebf36c8eef519204fd
Stefan Frank <s.frank@vierundsechzig.de>
2012-07-25 16:54:14 +0200

    #96 need to re-add the cookbooks

--- config/cookbooks/application/definitions/mysql.rb
+++
@@ -1,7 +0,0 @@
-define :mysql do
-  execute "performing query #{params[:name]}" do
-    password = params[:password] ? "--password='#{params[:password]}'" : ""
-    user = params[:user] || "root"
-    command %|mysql -u #{user} #{password} -e "#{params[:query]}"|
-  end
-end
