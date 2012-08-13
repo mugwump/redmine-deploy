97577f53dc7c831ca65a0791ec3330476b96b7cf
Michael Wöcherl <michael.woecherl@alere.com>
2012-08-13 14:19:15 +0200

    #218 adjusted settings of test_mw stage

--- config/deploy/test_mw.rb
+++ config/deploy/test_mw.rb
@@ -2,7 +2,7 @@ set :main_server,           "176.221.43.225"
 
 set :user,                  "root"
 set :password,              ""
-set :deploy_to,             "/home/ubuntu/deploy/#{application}-#{rails_env}"
+set :deploy_to,             "/opt/#{application}-#{rails_env}"
 
 set :url,                   "#{main_server}/redmine"
 set :email,                 "michael@woecherl.de" 
