86bf19b8258f30683433381743b0ca348d55c37c
Michael Wöcherl <michael.woecherl@alere.com>
2012-08-16 10:30:51 +0200

    #226 bug fix

--- config/deploy/parametrized.rb
+++ config/deploy/parametrized.rb
@@ -5,7 +5,7 @@ set :main_server,           ENV['REDMINE_DEPLOY_MACHINE']
 
 set :user,                  ENV['REDMINE_DEPLOY_USER']
 set :password,              ""
-set :deploy_to,             ENV['REDMINE_DEPLOY_PATH'] + "/#{application}-#{rails_env}"
+set :deploy_to,             "#{ENV['REDMINE_DEPLOY_PATH']}/#{application}-#{rails_env}"
 
 set :url,                   main_server + ENV['REDMINE_DEPLOY_REDMINE_URI']
 set :email,                 ENV['REDMINE_DEPLOY_EMAIL']
