dc37c623125c1f04924f4342dff467d097afb125
Stefan Frank <s.frank@vierundsechzig.de>
2013-02-20 13:35:55 +0100

    using a forked&patched version of roundsman to update chef to 11.4.0: the currently used 10.18.2 coughed up a dependency-issue with net-ssh. Also updated the vagrant -stage to work properly with the newly introduced mysql-variables.

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source 'http://rubygems.org'
 gem 'fog'
 gem 'capistrano'
 gem 'cape'
-gem 'roundsman', :require => false
+gem 'roundsman', :require => false, :git => "git@github.com:mugwump/roundsman.git"
 gem 'capistrano_colors', :git => "https://github.com/stjernstrom/capistrano_colors.git"
--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -5,7 +5,12 @@ set :password,              "vagrant"
 set :deploy_to,             "/home/vagrant/deploy/#{application}-#{rails_env}"
 
 set :url,                   "localhost"
-set :email,                 "s.frank@vierundsechzig.de" 
+set :email,                 "test@test.de"
+
+set :mysql, :root_pw  => "s00pers3cr3t",
+            :database => "redmine",
+            :username => "redmine",
+            :usr_pw   => "s3cr3t"
 
 server "#{main_server}",    :web, :app, :db, :primary => true
 
